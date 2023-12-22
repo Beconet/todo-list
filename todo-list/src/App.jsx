773e15dd3880e4a8d890782f39a7d27261a7c043
Beconet <psupacheep@gmail.com>
2023-12-22 18:58:01 +0700

    todo list v.1

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './app.css'
+
+function App() {
+  const [todoList , setTodoList] = useState('');
+  const [todoArray , setTodoArray] = useState([]);
+
+  const handleInputValue = (e) =>{
+    setTodoList(e.target.value);
+  }
+  const addTask = () => {
+    setTodoArray([...todoArray,todoList]);
+    setTodoList('');
+    
+  }
+  console.log(todoList)
+  console.log(todoArray);
+  return (
+      <>
+      <div className='container'>
+        <div>
+      <h1>To-Do List</h1>
+      <div>
+        <input type="text" placeholder='type here' onChange={handleInputValue} value={todoList}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+    
+    <div>
+      <ul>
+        {todoArray.map((todo,index)=>{
+         return(
+          <li key={index}>{todo}</li>
+         )
+        })}
+      </ul>
+    </div>
+    </div>
+    </div>
+      </>
+  )
+}
+
+export default App
